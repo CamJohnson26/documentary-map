884d960980f780f5ed82b559b42b388eff5537cc
Cameron Johnson <cameronrossjohnson@gmail.com>
2025-06-24 20:41:45 -0500

    ChatGPT code and list

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import MapClientWrapper from '../components/MapClientWrapper'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +8,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <MapClientWrapper meta={meta} />;
 }
